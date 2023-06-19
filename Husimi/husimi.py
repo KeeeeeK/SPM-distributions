8d92a6053af754114003a18adf45cdf5b7d224a9
Nougmanov Boulat <47330150+KeeeeeK@users.noreply.github.com>
2023-06-20 02:41:02 +0300

    added doc to husimi

--- Husimi/husimi.py
+++ Husimi/husimi.py
@@ -1,17 +1,26 @@
+from typing import Literal
 import numpy as np
 from F_normalized import Fn_sum, Fn_1b, Fn_2b, number_type
-from typing import Literal
 
 methods_str = Literal['sum': str, '1b': str, '2b': str]
 
 
-def husimi(alpha, beta_conj, gamma, method: methods_str = '1b', n_sigma: number_type | None = None):
+def husimi(alpha: complex | number_type,
+           beta_conj: complex | number_type,
+           gamma: number_type,
+           method: methods_str = '1b', n_sigma: number_type = 3) -> number_type:
+    """
+    :param alpha: parameter of coherence of initial state
+    :param beta_conj: conjugated beta, where beta is a parameter of husimi function
+    :param gamma: parameter of non-linearity. In the article it is named \Gamma
+    :param method: the method that will be used to find the values of the F normalized.
+     It should be on of 'sum' (direct summation), '1b' (1 branch), '2b' (2 branches).
+     For further details see the documentation in the F_normalized.py file
+    :param n_sigma: parameter that will be used only if method == 'sum'.
+    :return: the value of husimi function in point beta
+    """
     match method:
         case 'sum':
-            if n_sigma is None:
-                n_sigma = 3
-                Warning('When using the husimi calculation method by direct summation, it is necessary to specify the '
-                        'n_sigma parameter that affects the accuracy of summation.')
             return np.exp(-(np.abs(alpha) - np.abs(beta_conj)) ** 2) / np.pi * \
                 np.abs(Fn_sum(np.abs(alpha * beta_conj), 2 * gamma + np.angle(alpha * beta_conj), -gamma, n_sigma)) ** 2
         case '1b':
@@ -23,14 +32,17 @@ def husimi(alpha, beta_conj, gamma, method: methods_str = '1b', n_sigma: number_
         case _:
             raise ValueError('Unknown method')
 
+
 if __name__ == '__main__':
+    # This is a simple speed test of various methods.
+    # Using methods using branches of the Lambert function increases the speed by more than 100 times.
     from time import time
 
     alpha = 10 ** 3
     beta_conj = 10 ** 3 * np.exp(1j * np.linspace(0, 2 * np.pi, 10 ** 4))
     gamma = 10 ** -6
 
-    for method in ('sum', '1b', '2b'):
+    for method in ('1b', '2b', 'sum'):
         s = time()
         husimi(alpha, beta_conj, gamma, method=method)
         print(f'calc time of {method}: {time() - s} s')
