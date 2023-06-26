a5baa0f07ee76c1ac069cba459ee812d40c7a382
Nougmanov Boulat <47330150+KeeeeeK@users.noreply.github.com>
2023-06-26 03:08:40 +0300

    new k_max with bin search

--- Wigner/wigner.py
+++ Wigner/wigner.py
@@ -2,11 +2,12 @@ import numpy as np
 import numba as nb
 from SPM_distributions.Husimi.F_normalized import number_type
 
+
 @nb.vectorize('float64(float64, float64, float64, float64, float64, float64)',
               nopython=True, target_backend='parallel', fastmath=False)
 def wigner(alpha_abs: number_type, alpha_arg: number_type,
            beta_abs: number_type, beta_arg: number_type,
-           gamma: number_type, n_sigma: number_type) -> number_type:
+           gamma: number_type, tol=10 ** -3) -> number_type:
     """
     alpha is parameter of coherence of initial state
     :param alpha_abs: module of alpha
@@ -15,28 +16,38 @@ def wigner(alpha_abs: number_type, alpha_arg: number_type,
     :param beta_abs: module of beta
     :param beta_arg: angle of beta
     :param gamma: parameter of non-linearity. In the article it is named \Gamma
-    :param n_sigma: The terms in the limit of small gamma they have a Gaussian distribution. n_sigma is a parameter that
-     characterizes the number of standard deviations that will be taken into account when summing.
+    :param tol: the accuracy, less than 1
     :return: the value of wigner function in point beta
     """
     Phi0 = alpha_arg - beta_arg + gamma
     A = 4 * alpha_abs * beta_abs
-    norm = 2 * (alpha_abs - beta_abs) ** 2 + A
     # firstly we should know what to sum
-    if beta_abs < alpha_abs:
-        t_max = np.arccos(beta_abs / alpha_abs)
-        k_max = np.int_(t_max / gamma)
-        der2f = -1 / (4 * alpha_abs ** 2) - 4 * gamma ** 2 * (alpha_abs ** 2 - beta_abs ** 2) + t_max / 8 * \
-                (4 * np.sqrt(alpha_abs ** 2 - beta_abs ** 2) + beta_abs * t_max) / (alpha_abs ** 2 * beta_abs)
-    else:
-        k_max = 0
-        der2f = -1 / (4 * alpha_abs * beta_abs) + 4 * alpha_abs * (alpha_abs - beta_abs) * gamma ** 2
-    k_range = np.int_(1 / np.sqrt(-der2f) * n_sigma)
+    log_tol = np.log(tol)
+    left, mid, right = 0, np.pi / 4 / gamma, np.pi / 2 / gamma
+
+    def func(k):
+        z = A * np.exp(-1j * gamma * k)
+        return np.real(np.sqrt(z * z + k * k) - k * np.arcsinh(k / z)) - A + alpha_abs * alpha_abs * (
+                    1 - np.cos(2 * k * gamma))
+
+    # binary search algorithm
+    for i in range(np.int_(np.log2(np.pi / 2 / gamma))):
+        if func(mid) > log_tol:
+            left = mid
+        else:
+            right = mid
+        mid = (left + right) / 2
+    k_max = np.int_(mid)
     # now lets sum
     sum_fourier = 0
-    for k in nb.prange(-k_max - k_range, k_max + k_range + 1):
-        t = np.exp(1j * k * gamma)
+    norm = A + (beta_abs - alpha_abs) ** 2
+    for k in nb.prange(1, k_max + 1):
+        t = np.exp(-1j * k * gamma)
         z = A * t
-        V_k = np.sqrt(z * z + k * k) - k * np.log(k / z + np.sqrt(1 + (k / z) * (k / z))) - np.log(k * k + z * z) / 4
+        V_k = np.sqrt(z * z + k * k) - k * np.arcsinh(k / z) - np.log(k * k + z * z) / 4
         sum_fourier += np.exp(1j * k * Phi0 + V_k + alpha_abs * alpha_abs * (1 - t * t) - norm)
-    return 2 / np.pi / np.sqrt(2 * np.pi) * np.real(sum_fourier)
+    return 2 / np.pi / np.sqrt(2 * np.pi) * (2 * np.real(sum_fourier) - np.exp(A - norm) / np.sqrt(A))
+
+
+if __name__ == '__main__':
+    print(wigner(10 ** 3, 0, 10 ** 3, -2, 10 ** -6, 10 ** -2))
